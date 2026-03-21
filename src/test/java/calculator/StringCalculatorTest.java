4a0c3f7465a593088e98d77fddb0e0d40e8aac8a
monsaumon <monsaumon@naver.com>
2026-03-21 18:45:01 +0900

    test: 정규표현식 수정 관련 테스트 케이스 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -13,7 +13,7 @@ public class StringCalculatorTest {
 
     @DisplayName("구분자를 기준으로 분리한 숫자의 합을 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,22,3,44,5", "1:22:3:44:5", "1,22,3:44:5", "//;\n1,22;3:44;5", "//a\n1a22a3a44a5", "//a+|\n1+22|3a44,5"})
+    @ValueSource(strings = {"1,22,3,44,5", "1:22:3:44:5", "1,22,3:44:5", "//;\n1,22;3:44;5", "//a\n1a22a3a44a5", "//a[|\n1[22|3a44,5"})
     void testSumString(String value) {
         // given
 
@@ -26,7 +26,7 @@ public class StringCalculatorTest {
 
     @DisplayName("잘못된 형식이나 음수를 입력했을 때 RuntimeException을 throw한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1a2,3", "//a1a2a3", "//a\n-1a2a3", "//a\n1a-2a3", "//a\n1a2b3"})
+    @ValueSource(strings = {"1a2,3", "//a1a2a3", "//a\n-1a2a3", "//a\n1a-2a3", "//a\n1a2b3", "1[2[3"})
     void testSumStringException(String value) {
         // given
 
