dede2e3b5f05f28e6a220973178bbb47d4c3747e
monsaumon <monsaumon@naver.com>
2026-03-21 18:44:30 +0900

    fix: 정규표현식 생성 함수 수정 및 Pattern 사용
    [a|\\.|,|:]와 같은 정규표현식을 사용했었는데,
    [a.,:]가 올바른 사용

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,24 +1,21 @@
 package calculator;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     private int customSeparatorEndIndex;
-    private List<String> separators;
 
     // 각 과정을 순차적으로 실행
     public int sumString(String input) {
-        getSeparators(input);
-        String regex = generateRegexFromSeparators();
-        return splitAndSum(input, regex);
+        StringBuilder separators = getSeparators(input);
+        Pattern pattern = generatePatternFromSeparators(separators);
+        return splitAndSum(input, pattern);
     }
 
-    // 구분자 리스트 생성
-    private void getSeparators(String input) {
+    // 구분자 문자열 생성
+    private StringBuilder getSeparators(String input) {
         // default separators
-        separators = new ArrayList<>(Arrays.asList(",",":"));
+        StringBuilder separators = new StringBuilder(",:");
 
         // custom separators
         customSeparatorEndIndex = -1;
@@ -28,40 +25,45 @@ public class StringCalculator {
                 throw new RuntimeException("Missing '\\n' after custom separators");
             }
             for (int i = 2; i < customSeparatorEndIndex; i++) {
-                separators.add(Character.toString(input.charAt(i)));
-            }
-        }
-    }
-
-    // 구분자 리스트로부터 정규표현식 생성
-    private String generateRegexFromSeparators() {
-        // custom separator 중 정규표현식 메타 문자가 있다면 앞에 이스케이프 삽입
-        List<String> regexMetaSymbols = Arrays.asList("\\","^","$",".","|","[","]","(",")","*","+","?","{","}");
-        for (String regexMetaSymbol : regexMetaSymbols) {
-            int regexMetaSymbolIndex = separators.indexOf(regexMetaSymbol);
-            if (regexMetaSymbolIndex != -1) {
-                separators.set(regexMetaSymbolIndex, "\\"+regexMetaSymbol);
+                separators.append(input.charAt(i));
             }
         }
 
-        // 정규표현식 생성 및 반환
-        return "[" + String.join("|", separators) + "]";
+        return separators;
     }
 
-    // 구분자(정규표현식) 기준으로 문자열을 자르고 숫자 총합 구하기
-    private int splitAndSum(String input, String regex) {
+    // 구분자 문자열로부터 Pattern 생성
+    private Pattern generatePatternFromSeparators(StringBuilder separators) {
+        // 구분자 중 대괄호가 있다면 이스케이프 (정규표현식 오류 방지)
+        int leftBracketIndex = separators.indexOf("[");
+        if (leftBracketIndex != -1) {
+            separators.insert(leftBracketIndex, "\\");
+        }
+        int rightBracketIndex = separators.indexOf("]");
+        if (rightBracketIndex != -1) {
+            separators.insert(rightBracketIndex, "\\");
+        }
+
+        // Pattern 생성 및 반환
+        return Pattern.compile("[" + separators + "]");
+    }
+
+    // Pattern에 따라 문자열을 분할하고 숫자 총합 구하기
+    private int splitAndSum(String input, Pattern pattern) {
         // custom separator 지정 부분 잘라내기
         String numString = input.substring(customSeparatorEndIndex + 1);
 
         // 정규표현식 기준으로 나누기
-        String[] splitString = numString.split(regex);
+        String[] splitString = pattern.split(numString);
 
         // 분할된 숫자 총합 구하기
         int sum = 0;
         for (String str: splitString) {
             try{
                 int num = Integer.parseInt(str);
-                if (num < 0) throw new RuntimeException("Do not input negative numbers");
+                if (num < 0) {
+                    throw new RuntimeException("Do not input negative numbers");
+                }
                 sum += num;
             } catch (NumberFormatException e) {
                 throw new RuntimeException("Please input only numbers and separators");
