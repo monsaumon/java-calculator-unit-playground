4ebf20c5ff839f4931cce66e24ef4b228ce6cacf
monsaumon <monsaumon@naver.com>
2026-03-23 00:46:35 +0900

    feat: 0으로 나누기 에러 메시지 추가

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -14,6 +14,9 @@ public class Calculator {
     }
 
     public int divide(int num1, int num2) {
+        if(num2 == 0) {
+            throw new ArithmeticException("0으로 나눌 수 없습니다.");
+        }
         return num1 / num2;
     }
 }
